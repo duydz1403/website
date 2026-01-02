c67bfac9ef0f360b08fdbb4755f9b5b835bbbf14
Nhat Duy <duydz1403@gmail.com>
2026-01-02 14:18:31 +0700

    hoan thien website ca nhan

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function sayHello() {
     document.getElementById("message").innerText =
         "🎉 Chào mừng bạn đến với website cá nhân của mình!";
+    document.getElementById("darkToggle").onclick = () => {
+    document.body.classList.toggle("dark");
+};
+    
 }
